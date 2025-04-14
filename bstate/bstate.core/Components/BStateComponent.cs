b4724235168a4e6f94ca710e8bd0da3aa22b860d
Marco Milani <emme.milani@hotmail.it>
2025-04-14 15:40:48 +0200

    Add state registration and test component scaffold
    Introduced automatic discovery and registration of `BState`-derived classes in `AddBState`. Added a basic `TestComponent` for demonstration and updated `BStateComponent` with a placeholder `UseState<T>` method. These changes lay the groundwork for state management and enhance component extensibility.

--- bstate/bstate.core/Components/BStateComponent.cs
+++ bstate/bstate.core/Components/BStateComponent.cs
@@ -4,5 +4,8 @@ namespace bstate.core.Components;
 
 public abstract class BStateComponent : ComponentBase
 {
-    
+    protected void UseState<T>() where T : BState
+    {
+           
+    }
 }
--- bstate/bstate.core/Startup.cs
+++ bstate/bstate.core/Startup.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Linq;
 
 namespace bstate.core;
 
@@ -6,6 +8,19 @@ public static class Startup
 {
     public static void AddBState(IServiceCollection serviceCollection)
     {
-        
-    } 
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        RegisterStates(serviceCollection, assemblies);
+    }
+
+    private static void RegisterStates(IServiceCollection serviceCollection, Assembly[] assemblies)
+    {
+        var bstateTypes = assemblies
+            .SelectMany(a => a.GetTypes())
+            .Where(t => t is { IsClass: true, IsAbstract: false } && t.IsSubclassOf(typeof(BState)));
+
+        foreach (var type in bstateTypes)
+        {
+            serviceCollection.AddSingleton(type);
+        }
+    }
 }
