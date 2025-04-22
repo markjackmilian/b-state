a0223735188220baed423e3ed7c00111c605aecc
Marco Milani <emme.milani@hotmail.it>
2025-04-22 16:45:22 +0200

    Refactor WeatherInitialize to remove redundant property.
    Simplified the class by removing the unnecessary private property `State` and directly using the constructor parameter `state`. This improves code clarity and reduces redundancy.

--- bstate/bstate.web.example/Features/Weather/WeatherInitialize.cs
+++ bstate/bstate.web.example/Features/Weather/WeatherInitialize.cs
@@ -8,11 +8,10 @@ namespace bstate.web.example.Features.Weather;
 
 class WeatherInitialize(HttpClient httpClient, WeatherState state) : IOnInitialize
 {
-    private WeatherState State => state;
     public async Task OnInitialize(BStateComponent component)
     {
         await Task.Delay(2000); // simulate async call ( fetch data from server)
         var forecasts = await httpClient.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
-        await State.SetForecasts(forecasts);
+        await state.SetForecasts(forecasts);
     }
 }
