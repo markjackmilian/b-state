42a94b413ac1a4bc779440973a15766d5dac4248
Marco Milani <emme.milani@hotmail.it>
2025-05-03 09:50:31 +0200

    Optimize ActionRunnerMiddleware with caching for performance
    Introduced caching for handler and method reflection using `ConcurrentDictionary` to improve performance. This reduces redundant computations when resolving handlers and methods during action execution.

--- bstate/bstate.core/Middlewares/ActionRunnerMiddleware.cs
+++ bstate/bstate.core/Middlewares/ActionRunnerMiddleware.cs
@@ -1,23 +1,33 @@
-using System.Diagnostics;
 using bstate.core.Classes;
 using PipelineNet.Middleware;
+using System.Collections.Concurrent;
+using System.Reflection;
 
 namespace bstate.core.Middlewares;
 
 public class ActionRunnerMiddleware(IServiceProvider serviceProvider) : IAsyncMiddleware<IAction>
 {
+    private static readonly ConcurrentDictionary<Type, Type> HandlerTypeCache = new();
+    private static readonly ConcurrentDictionary<Type, MethodInfo> ExecuteMethodCache = new();
+
     public async Task Run(IAction parameter, Func<IAction, Task> next)
     {
         var parameterType = parameter.GetType();
-        var handlerType = typeof(IActionHandler<>).MakeGenericType(parameterType);
+
+        var handlerType = HandlerTypeCache.GetOrAdd(parameterType, type =>
+            typeof(IActionHandler<>).MakeGenericType(type));
+
         var handler = serviceProvider.GetService(handlerType);
+
         if (handler != null)
         {
-            var executeMethod = handlerType.GetMethod("Execute");
+            var executeMethod = ExecuteMethodCache.GetOrAdd(handlerType, type =>
+                type.GetMethod("Execute"));
+
             var task = (Task)executeMethod!.Invoke(handler, [parameter]);
             await task!;
         }
-    
+
         await next(parameter);
     }
 }
